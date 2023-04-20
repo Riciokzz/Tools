51f8574c1db168a79463cd38ba0840b5d71b42ae
Riciokzz <ricardas.poskrebysev@gmail.com>
2023-04-20 20:34:33 +0300

    Set window geometry

--- programs/calculator/calc.py
+++ programs/calculator/calc.py
@@ -1,5 +1,31 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
-root.title("Calculator")
-root.mainloop()
+calculation = ""
+
+
+def add_calculation(symbol):
+    pass
+
+
+def eval_calculation():
+    pass
+
+
+def clear_field():
+    pass
+
+
+# Calculator window setup
+
+win = tk.Tk()
+win.geometry("300x275")
+win.title("Calculator")
+
+# Display frame
+text_result = tk.Text(win, height=2, width=16, font=("Arial", 24, "bold"))
+text_result.grid(columnspan=5)
+text_result.pack()
+
+# Buttons frame
+
+win.mainloop()
