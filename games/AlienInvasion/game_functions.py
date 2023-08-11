1f011a3f513671bcfe77a41e19e28869441b85ae
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-08-11 12:51:43 +0300

    refract game functions add functions for keypress and release

--- games/AlienInvasion/game_functions.py
+++ games/AlienInvasion/game_functions.py
@@ -3,24 +3,32 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """Respond to keypress."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to key release."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Respond to key presses and mouse evens."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move the ship to the right.
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event=event, ship=ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event=event, ship=ship)
+
 
 
 def update_screen(ai_settings, screen, ship, game_character):
