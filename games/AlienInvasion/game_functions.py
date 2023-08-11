7507d0c0cbcaa55ed228e06f730df13b38d8472f
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-08-11 14:41:38 +0300

    limit player - use 3 bullets at once

--- games/AlienInvasion/game_functions.py
+++ games/AlienInvasion/game_functions.py
@@ -11,9 +11,10 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullers group.
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        # Create a new bullet and add it to the bullets group.
+        if len(bullets) < ai_settings.bullet_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
