488d99be4a78540da544334af35bd7b1970143bd
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-05 11:18:23 +0300

    add ship parameter to update_aliens

--- games/AlienInvasion/alien_invasion.py
+++ games/AlienInvasion/alien_invasion.py
@@ -34,7 +34,7 @@ def run_game():
         gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
 
         # Update aliens on screen.
-        gf.update_aliens(ai_settings, aliens)
+        gf.update_aliens(ai_settings, ship, aliens)
 
         # Update screen base by previous updates.
         gf.update_screen(ai_settings=ai_settings, screen=screen, ship=ship, aliens=aliens, bullets=bullets)
