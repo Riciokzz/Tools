e66558db9e5ce53d7879dd7bf65a685378f079c1
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-05 15:11:03 +0300

    add draw_button function - call function to display button

--- games/AlienInvasion/button.py
+++ games/AlienInvasion/button.py
@@ -22,11 +22,16 @@ class Button:
         self.prep_msg(msg)
 
 
-def prep_msg(self, msg):
-    """Turn msg into a rendered image and center text on the button."""
-    self.msg_image = self.font.render(msg,
-                                      True,
-                                      self.text_color,
-                                      self.button_color)
-    self.msg_image_rect = self.msg_image.get_rect()
-    self.msg_image_rect.center = self.rect.center
+    def prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg,
+                                          True,
+                                          self.text_color,
+                                          self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # Draw blank button and then draw message.
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
