ddcd4f38440eeff1ed056a628031e872e355d003
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-05 15:44:36 +0300

    update check_events function - add mosuebuttondown add check_play_button function

--- games/AlienInvasion/game_functions.py
+++ games/AlienInvasion/game_functions.py
@@ -70,7 +70,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship, ai_settings, screen, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Respond to key presses and mouse evens."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -83,6 +83,14 @@ def check_events(ship, ai_settings, screen, bullets):
                                  bullets=bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event=event, ship=ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+def check_play_button(stats, play_game, mouse_x, mouse_y):
+    """Start a new game when the player click Play."""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 
 def check_fleet_edges(ai_settings, aliens):
