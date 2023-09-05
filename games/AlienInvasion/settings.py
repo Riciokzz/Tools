ca0a934b356f1177c14edcedd2392b8df097d4c9
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-05 10:48:20 +0300

    change speed of aliens

--- games/AlienInvasion/settings.py
+++ games/AlienInvasion/settings.py
@@ -19,7 +19,7 @@ class Settings:
         self.bullet_allowed = 3
 
         # Alien settings
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 0.5
         self.fleet_drop_speed = 10
         # fleet direction represents + right , - left.
         self.fleet_direction = 1
