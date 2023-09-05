194f9153f75d81ad784363a53c8584403ffa511c
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-05 10:23:59 +0300

    add update function

--- games/AlienInvasion/alien.py
+++ games/AlienInvasion/alien.py
@@ -22,7 +22,11 @@ class Alien(Sprite):
         # Store the alien's exact position.
         self.x = float(self.rect.x)
 
-
     def blitme(self):
         """Draw the alien at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Move the alien right."""
+        self.x += self.ai_settings.alien_speed_factor
+        self.rect.x = self.x
