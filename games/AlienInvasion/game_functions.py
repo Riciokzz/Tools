98f9110806b3591c5fc4a5d67b29d257abe1e4f6
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-05 11:14:32 +0300

    refactor game_functions.py add check_bullet_alien_collisions function

--- games/AlienInvasion/game_functions.py
+++ games/AlienInvasion/game_functions.py
@@ -124,12 +124,16 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-    # Check for any bullets that have hit aliens.
-    # If True get rid of hte bullet and the alien.
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """Respond to bullet-alien collisions."""
+    # Remove any bullets and aliens that have collided.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # Destroy existing bullets and create new fleet.
+        # Destroy existing bullets ad create new fleet.
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
 
