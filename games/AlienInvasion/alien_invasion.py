2348bcd307daaaf9e14e233276323e19994ae6b3
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-06 16:50:10 +0300

    add sb parameter to update_aliens()

--- games/AlienInvasion/alien_invasion.py
+++ games/AlienInvasion/alien_invasion.py
@@ -52,7 +52,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
             # Update aliens on screen.
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
         # Update screen base by previous updates.
         gf.update_screen(ai_settings=ai_settings,
