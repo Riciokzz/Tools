078b18d1fddcae86a11ae0fa893dd87ba5facbc1
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-06 16:29:25 +0300

    import Sprite - ship inherit Sprite

--- games/AlienInvasion/ship.py
+++ games/AlienInvasion/ship.py
@@ -2,10 +2,11 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Ship:
+class Ship(Sprite):
 
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting place."""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
