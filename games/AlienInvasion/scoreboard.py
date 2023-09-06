ff1d3589b0915db3802d19209899506eed5a13c1
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-06 16:35:18 +0300

    add prep_ships() - to hold ship instance add loop every ship left, inside loop create new ship and place at position add created ship to group

--- games/AlienInvasion/scoreboard.py
+++ games/AlienInvasion/scoreboard.py
@@ -47,6 +47,15 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = int(round(self.stats.high_score, -1))
