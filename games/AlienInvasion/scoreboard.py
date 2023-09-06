98eb5e91695b1d5815f8f18205a915e449574c80
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-06 17:28:49 +0300

    fix typo

--- games/AlienInvasion/scoreboard.py
+++ games/AlienInvasion/scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard:
     """A class to report scoring information."""
 
     def __init__(self, ai_settings, screen, stats):
-        """Initialize scorekeeping attributes."""
+        """Initialize score keeping attributes."""
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
