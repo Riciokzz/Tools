946b7e6558eda37c0c25dae74f246a3d18c66a33
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-06 16:10:54 +0300

    add call prep_level() create prep_level()

--- games/AlienInvasion/scoreboard.py
+++ games/AlienInvasion/scoreboard.py
@@ -18,6 +18,16 @@ class Scoreboard:
         # Prepare the initial score images.
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
+
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        self.level_image = self.font.render(str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
+
+        # Position the level below the score.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_score(self):
         """Turn the score into rendered image."""
