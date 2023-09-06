96d4207fa05e36ff326a39c6e16eadfe074c2e95
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-06 08:48:48 +0300

    fix typo

--- games/AlienInvasion/alien_invasion.py
+++ games/AlienInvasion/alien_invasion.py
@@ -21,7 +21,7 @@ def run_game():
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
 
-    # Make a ship, a goupr of bullets, and a group of aliens.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings=ai_settings, screen=screen)
     bullets = Group()
     aliens = Group()
