f0d8a679410ca7cd04f8518602c800373cef26a8
Ričardas Poskrebyševas <ricardas.poskrebysev@gmail.com>
2023-09-06 16:12:46 +0300

    add level image to show_score()

--- games/AlienInvasion/scoreboard.py
+++ games/AlienInvasion/scoreboard.py
@@ -55,6 +55,7 @@ class Scoreboard:
         self.high_score_rect.top = self.score_rect.top
 
     def show_score(self):
-        """Draw score to the screen."""
+        """Draw scores and the ships to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
